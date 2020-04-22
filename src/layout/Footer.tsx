dea061144a8720a585ed97c56828c5325dcad4d6
christian quispe <christianquispecamasca@gmail.com>
2020-04-21 20:03:00 -0500

    chore: create footer component

---
+++ src/layout/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+const Footer: React.FC = (props) => {
+  return (
+    <footer>
+      <div>
+        <h2>Footer</h2>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
